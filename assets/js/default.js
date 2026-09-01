d18e1c9a67271b7f3aae5de1799900fd86047b5e
GrntKim <grantjun0224@gmail.com>
2026-09-01 16:05:56 +0900

    delete unused script file

--- assets/js/default.js
+++
@@ -1,8 +0,0 @@
-const sidebar = document.getElementById("sidebar");
-const overlay = document.getElementById("sidebar-overlay");
-
-function toggleSidebar() {
-  sidebar.classList.toggle('-translate-x-full');
-  overlay.classList.toggle('opacity-0');
-  overlay.classList.toggle('pointer-events-none');
-}
